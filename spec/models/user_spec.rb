36d365533d6110bb832d6361dc78dc1b5965d90e
Unknown <lincolnsrjr@gmail.com>
2017-10-12 20:14:08 -0300

    Adionando a gem capybara

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  context "Test view user" do
+    it "Same view test" do
+      visit "/users/new"
+
+      within("form") do
+
+      end
+
+      click_button 'Create User'
+      expect(page).to have_content 'User was successfully created.'
+
+    end
+  end
+  
   context "Same context" do
     it "Same test" do
       expect(true).to be_truthy
